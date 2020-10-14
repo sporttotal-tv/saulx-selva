3823950bf9f9faa2e7792a738d1532030e110790
Olli Vanhoja <olli.vanhoja@gmail.com>
2020-10-15 01:05:13 +0200

    Add missing header

---
+++ server/modules/selva/module/selva_object.h
@@ -0,0 +1,13 @@
+#pragma once
+#ifndef SELVA_OBJECT
+#define SELVA_OBJECT
+
+struct SelvaObject;
+struct RedisModuleString;
+
+int SelvaObject_Key2Obj(RedisModuleKey *key, struct SelvaObject **out);
+int SelvaObject_Exists(struct SelvaObject *obj, const RedisModuleString *key_name);
+int SelvaObject_GetStr(struct SelvaObject *obj, const RedisModuleString *key_name, RedisModuleString **out);
+int SelvaObject_SetStr(struct SelvaObject *obj, const RedisModuleString *key_name, RedisModuleString *value);
+
+#endif /* SELVA_OBJECT */
